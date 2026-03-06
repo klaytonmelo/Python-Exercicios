b89dfedaaa53f41ad8e78ceafd0e34ddc7f923b7
Klayton de Melo <klaytonmelo09@gmail.com>
2026-03-06 16:00:15 -0300

    ex038 finalizado

--- modulo02/ex038.py
+++ modulo02/ex038.py
@@ -1,9 +1,9 @@
-n1 = int(input("Digite um numero inteiro: "))
-n2 = int(input("Digite outro numero inteiro: "))
+n1 = int(input("primeiro numero: "))
+n2 = int(input("Segundo numero: "))
 
 if n1 > n2:
-    print(f" O primeiro valor é maior")
+    print("O Primeiro valor é maior!")
 elif n2 > n1:
-    print("O segundo valor é maior")
+    print("O segundo valor é maior!")
 else:
-    print("Não existe valor maior, os dois são iguais")
+    print("Os dois valores são iguais!")
