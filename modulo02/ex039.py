d6b761543d7ba3aa9a188f825ed30a65f40359a2
Klayton de Melo <klaytonmelo09@gmail.com>
2026-03-06 17:20:47 -0300

    ex039 finalizado

--- modulo02/ex039.py
+++ modulo02/ex039.py
@@ -1,7 +1,16 @@
 import datetime
 ano_atual = datetime.date.today().year
-data_n = int(input("ano de nascimento: "))
+data_n = int(input("Ano de nascimento: "))
 
 idade = ano_atual - data_n
 
-print(f"quem nasceu em {data_n} tem {idade} anos")
+print(f"quem nasceu em {data_n} tem {idade} em {ano_atual}")
+
+if idade == 18:
+    print("Você tem que se alistar imediatamete!")
+elif(idade < 18):
+    print(f"Você ainda não tem 18 anos. Ainda faltam {18 - idade} para o alistamento!")
+    print(f"Seu alistamento será em {ano_atual + (18 - idade)}")
+else:
+    print(f"Você ja deveria ter se alistado a {idade - 18} anos!")
+    print(f"seu alistamento foi em {ano_atual - (idade - 18)}")
