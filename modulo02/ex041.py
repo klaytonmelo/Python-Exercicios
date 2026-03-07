4bf0b0067566234282996d5fba38d72935e27925
Klayton de Melo <klaytonmelo09@gmail.com>
2026-03-07 18:39:13 -0300

    ex041 finalizado

--- modulo02/ex041.py
+++ modulo02/ex041.py
@@ -1,5 +1,21 @@
-import datetime
+#import datetime
 from datetime import date
 
 ano_atual = date.today().year
 
+ano_nascimento = int(input("Ano de nascimento: "))
+
+idade = ano_atual - ano_nascimento
+
+print(f"O atleta tem {idade} anos.")
+
+if idade <= 9:
+    print("Classificação: MIRIM")
+elif idade <= 14:
+    print("Classificação: INFANTIL")
+elif idade <= 19:
+    print("Classificação: JUNIOR")
+elif idade <= 25:
+    print("Classificação: SÊNIOR")
+else:
+    print("Classificação: MASTER")    
