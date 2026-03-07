cb79f0b9f5cd785da71614f4c1073a97d5587dce
Klayton de Melo <klaytonmelo09@gmail.com>
2026-03-07 19:11:17 -0300

    ex042 finalizado

--- modulo02/ex042.py
+++ modulo02/ex042.py
@@ -1,16 +1,19 @@
-print("-="*20)
-print("Analizador de Triangulos")
-print("-="*20)
-r1 = float(input("Primeiro segmento: "))
-r2 = float(input("segundo segmento: "))
-r3 = float(input("Terceiro segmento: "))
-if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print("Os segmentos acima podem formar triangulos")
-    if r1 == r2 == r3:
-        print("EQUILATERO")
-    elif r1 == r2 or r1 == r3 or r2 == r3:
-        print("Isósceles")
+s1 = int(input("primeiro seguimento: "))
+s2 = int(input("Segundo seguimento: "))
+s3 = int(input("Terceiro seguimento: "))
+
+if s1 < s2 + s3 and s2 < s1 +s3 and s3 < s1 +s2:
+    print("Os seguimentos acima podem formar um triangulo!")
+
+    #equilatero -- todos os lados iguais
+    if s1 == s2 and s1 == s3 and s3 == s1:
+        print("Os seguimentos formam um triangulo EQUILATERO!")
+    #isoscelis -- dois lados iguais
+    elif s1 == s2 or s1 == s3 or s2 == s3:
+        print("Os seguimentos formam um triangulo ISOSCELIS")
+    #escaleno -- todos os lados diferentes
     else:
-        print("ESCALENO")
+        print("Os seguimentos formam um triangulo ESCALENO")
+
 else:
-    print("Os segmentos acima não podem formar triangulos")
+    print("Os seguimentos acima não podem formar um triangulo!")
