55b8a9440e8d5725f0ca92ffaa64fe0b1392e513
Klayton de Melo <klaytonmelo09@gmail.com>
2026-03-08 10:50:50 -0300

    ex043 finalizado!

--- modulo02/ex043.py
+++ modulo02/ex043.py
@@ -1,17 +1,18 @@
+print("*** Veja seu IMC ***")
+
 p = float(input("Digite seu peso: "))
 a = float(input("Digite sua altura: "))
 
-imc = p / (a**2)
+imc = p / (a ** 2)
 
-print(f"Seu IMC é {imc:.2f}")
-
+print(f"O IMC dessa pessoa é {imc:.1f}")
 if imc < 18.5:
-    print("abaixo do peso")
+    print("peso BAIXO")
 elif imc < 25:
-    print("peso ideal")
+    print("Peso IDEAL")
 elif imc < 30:
-    print("sobrepeso")
-elif imc <= 40:
-    print("obesidade")
+    print("SobrePESO")
+elif imc < 40:
+    print("OBESIDADE")
 else:
-    print("obesidade morbida")
+    print("Obesidade MÓRBIDA")
