a932097cc11cd1b348d785ef367e3995569c7e28
Klayton de Melo <klaytonmelo09@gmail.com>
2026-03-08 11:02:36 -0300

    Update ex044.py

--- modulo02/ex044.py
+++ modulo02/ex044.py
@@ -1,13 +1,10 @@
-p = float(input("Digite o preço do produto:R$"))
+print("====== Lojas python ======")
+p = float(input("Digite o preço das compras: "))
 
-print("formas de pagamento")
-print("(1) - à vista: dinheiro/cheque")#10% de desconto
-print("(2) - à vista no cartão")#5% de desconto
-print("(3) - Em até 2x no cartão")#preço normal
-print("(4) - 3x ou mais no cartão")#20% de juros
+print("===== FORMAS DE PAGAMENTO =====")
+print("[ 1 ] à vista dinhairo/cheque")
+print("[ 2 ] à vista cartão")
+print("[ 3 ] 2x no cartão")
+print("[ 4 ] 3x ou mais no cartão")
 
-fp = int(input("Digite a opção corespondente a forma de pagamento:\n"))
-if fp == 1:
-    desconto = p - (p * 0.10)
-    print("seu desconto é de 10%")
-    print(f"O produto que custava R${p} ficou R${desconto}")
+op = str(input("escolha a opção: "))
