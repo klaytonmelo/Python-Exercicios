030381d5db423c49c857a1148ad7772c79e10071
Klayton de Melo <klaytonmelo09@gmail.com>
2026-03-29 12:45:03 -0300

    ex047 finalizado!

---
+++ modulo02/ex047.py
@@ -0,0 +1,9 @@
+'''
+crie um programa que mostre na tela todos os 
+números pares que estão no intervalo de 1 e 50
+'''
+print("numeros pares no intervalo de 1 a 50")
+for c in range(1, 50):
+    if c % 2 == 0:
+        print(c)
+    
