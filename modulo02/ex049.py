f6b1687a74b64888536c4626ea888d35f85f0260
Klayton de Melo <klaytonmelo09@gmail.com>
2026-04-01 18:10:14 -0300

    ex049 finalizado!

--- modulo02/ex049.py
+++ modulo02/ex049.py
@@ -2,4 +2,11 @@
 Refaça o DESAFIO 009, mostrando a tabuada de um numero
 que o usuário escolher, só que agora utilizando um laço for.
 '''
+from time import sleep
 
+n = int(input("Digite um numero para ver sua tabuada:\n"))
+
+for c in range(1,11):
+    print(f"{n} X {c} = {n*c}")
+    sleep(1)
+    
