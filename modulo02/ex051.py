b896ad5d96a8bedac86655f217905e25306e6b34
klaytonmelo <klaytonmelo09@gmail.com>
2026-06-29 18:30:18 -0300

    ex051 finalizado!!

--- modulo02/ex051.py
+++ modulo02/ex051.py
@@ -3,4 +3,15 @@ Desenvolva um programa que leia o primeiro termo
 e a razão de uma PA. No final, mostre os 10 primeiros termos
 dessa progressão.
 '''
+print(40 * '=')
+print("   10 termos de uma PA")
+print(40 * '=')
 
+pt = int(input('Primeiro termo: '))
+rasao = int(input('Rasão: '))
+decimo = pt + (10 - 1) * rasao
+
+for c in range(pt, decimo + rasao, rasao):
+    print(f"{c} →")
+
+print("Acabou")
