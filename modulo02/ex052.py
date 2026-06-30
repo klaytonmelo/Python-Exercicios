e4c3dfa3ad8131c10ab007f0a8bc664a28fc8975
klaytonmelo <klaytonmelo09@gmail.com>
2026-06-30 20:02:13 -0300

    ex052 finalizando!

--- modulo02/ex052.py
+++ modulo02/ex052.py
@@ -7,3 +7,16 @@ DICA: numeros primos são aqueles que são apenas
 divisiveis por um e por ele mesmo.
 ***********
 '''
+from rich import print #biblioteca rich está no mundo 4 - "python POO"
+
+n = int(input('Digite um número: '))
+
+tot = 0
+for c in range(1, n + 1):
+    if n % c == 0:
+        print(f"[yellow]{c}[/]")
+        tot += 1
+    else:
+        print(f"[red]{c}[/]")
+
+print(f"O número {n}, foi divisivel {tot} vezes")
