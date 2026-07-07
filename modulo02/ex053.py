a6e4bce8a7e1b0f66de6925574d5b91b1d350465
klaytonmelo <klaytonmelo09@gmail.com>
2026-07-07 14:25:40 -0300

    ex053 finalizado!

--- modulo02/ex053.py
+++ modulo02/ex053.py
@@ -15,3 +15,18 @@ ex: APOS A SOPA
 
 ***********
 '''
+
+frase = str(input('Digite uma frase: ')).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+print(f"Você digitou a frase {junto}")
+
+inverso = ""
+for letra in range(len(junto) - 1, -1, -1):
+    inverso += junto[letra]
+
+print(f"O inverso de {junto} é {inverso}")
+if inverso == junto:
+    print("Temos um palídromo")
+else:
+    print("A frase digitada não é um palídromo!")
