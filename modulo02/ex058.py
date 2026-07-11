eaac4b0d4bc40700b212e186e7d82f2c4d493804
klaytonmelo <klaytonmelo09@gmail.com>
2026-07-11 20:58:13 -0300

    ex058 finalizado!

--- modulo02/ex058.py
+++ modulo02/ex058.py
@@ -4,3 +4,29 @@ um número inteiro entre 0 e 10. Só que agora o jogador vai tentar
 adivinhar até acertar, mostrando no final quantos palpites foram
 nescessários para vencer.
 '''
+from random import randint
+from time import sleep
+from rich import print # biblioteca no modulo 4 - POO
+
+computador = randint(0,5) #faz o computador "PENSAR"
+
+print("-=-" * 20)
+print("[blue]vou pensar em um numero entre 0 e 5, tente adivinhar...[/]")
+print("-=-" * 20)
+
+jogador = int(input('Em que número eu pensei? '))
+print("[green]processando...[/]")
+sleep(3)
+
+palpites = 0
+while jogador != computador:
+    palpites += 1
+    print("[red]Você errou!! tente novamente![/]")
+    jogador = int(input('Em que número eu pensei? '))
+    print("[green]processando...[green]")
+    sleep(2)
+
+print("[blue]Parabéns você acertou!!![/]")
+print(15*"[black]-=[/]")
+print(f"foram nescessarios {palpites} palpites para você vencer!")
+print(15*"[black]-=[/]")
