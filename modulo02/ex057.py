45d09eee7ad7e3c0d9bcb670c746d21f4c24a760
klaytonmelo <klaytonmelo09@gmail.com>
2026-07-11 20:38:26 -0300

    ex057 finalizado!

--- modulo02/ex057.py
+++ modulo02/ex057.py
@@ -3,3 +3,12 @@ Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores
 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor 
 correto..
 '''
+from rich import print# biblioteca rich está no módulo 4 - POO
+
+sexo = str(input('Digite seu sexo [M/F]: '))
+
+while sexo not in "MFfm":
+    print("[red]As informações estão incorretas![/red]")
+    sexo = str(input('Digite seu sexo [M/F]'))
+
+print("[blue]informações corretas!\nObrigado![/blue]")
