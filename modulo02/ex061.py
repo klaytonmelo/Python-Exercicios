53a76fcfb63934b1ef3e4d1d4273f81ec4b9663a
klaytonmelo <klaytonmelo09@gmail.com>
2026-08-03 20:08:29 -0300

    ex061 finalizado!

--- modulo02/ex061.py
+++ modulo02/ex061.py
@@ -3,3 +3,28 @@ Refaça o desafio 051, lendo o primeiro termo e a razão de uma
 PA, mostrando os 10 primeiros termos da progressão usando a
 estrutura while.
 '''
+
+print(40 * '=')
+print(" Gerador de PA ")
+print(40 * '=')
+
+pt = int(input('Primeiro termo: '))
+rasao = int(input('Rasão: '))
+
+termo = pt
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print(f"{termo} -> ", end="")
+        termo += rasao
+        cont += 1
+
+    print("Pausa")
+
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+
+print("Fim")
+print(f"progressão finalizada com {total} termos mostrados")
