139e263655f67dd55ac2ef0aeab6d3d9dd59709f
klaytonmelo <klaytonmelo09@gmail.com>
2026-08-03 21:32:35 -0300

    update!

--- modulo02/ex061.py
+++ modulo02/ex061.py
@@ -13,18 +13,9 @@ rasao = int(input('Rasão: '))
 
 termo = pt
 cont = 1
-total = 0
-mais = 10
-while mais != 0:
-    total += mais
-    while cont <= total:
-        print(f"{termo} -> ", end="")
-        termo += rasao
-        cont += 1
-
-    print("Pausa")
-
-    mais = int(input('Quantos termos você quer mostrar a mais? '))
+while cont <= 10:
+    print(f"{termo} -> ", end="")
+    termo += rasao
+    cont += 1
 
 print("Fim")
-print(f"progressão finalizada com {total} termos mostrados")
--- modulo02/ex062.py
+++ modulo02/ex062.py
@@ -4,3 +4,28 @@ usuário se ele quer mostrar mais alguns
 termos. O programa encerra quando ele
 disser que quer mostrar 0 termos.
 '''
+
+print(40 * '=')
+print(" Gerador de PA ")
+print(40 * '=')
+
+pt = int(input('Primeiro termo: '))
+rasao = int(input('Rasão: '))
+
+termo = pt
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total += mais
+    while cont <= total:
+        print(f"{termo} -> ", end="")
+        termo += rasao
+        cont += 1
+
+    print("Pausa")
+
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+
+print("Fim")
+print(f"progressão finalizada com {total} termos mostrados")
