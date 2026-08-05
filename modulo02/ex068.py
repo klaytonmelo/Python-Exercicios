c43544b37f10b54780f3ab7cdff944b0ef957467
klaytonmelo <klaytonmelo09@gmail.com>
2026-08-05 15:14:36 -0300

    Update ex068.py

--- modulo02/ex068.py
+++ modulo02/ex068.py
@@ -6,17 +6,21 @@ de vitórias consecutivas que ele conquistou no final do jogo!
 from random import randint
 from rich import print
 
+print("[yellow]-=[/]" * 20)
+print("[green]Vamos jogar PAR ou IMPAR[/]")
+print("[yellow]-=[/]" * 20)
+vitoria = 0
 while True:
-    print("[yellow]-=[/]" * 20)
-    print("[green]Vamos jogar PAR ou IMPAR[/]")
-    print("[yellow]-=[/]" * 20)
-
-    jogador = int(input('Diga um valor'))
+    jogador = int(input('Diga um valor: '))
     p_i = str(input('Par ou Impar? [P/I]: '))
 
-    if p_i in "Pp":
+    while p_i.strip().upper()[0] not in ["P", "I"]:
+        print("[red]ERRO![/] [green]digite novamente...[/]")
+        p_i = str(input('Par ou Impar? [P/I]: '))
+
+    if p_i.upper() in ["P","PAR"]:
         p_i = "PAR"
-    elif p_i in "Ii":
+    elif p_i.upper() in ["I","IMPAR"]:
         p_i = "IMPAR"
 
     computador = randint(0, 100)
@@ -28,10 +32,15 @@ while True:
         op = "IMPAR"
 
     print(f"você jogou {jogador} e o [yellow]computador jogou {computador}[/]. Total: {soma} [blue]Resultado: {op}[/]")
+
     if op == p_i:
-        print("[blue]Você venceu[/]")
+        print("[yellow]-=[/]" * 20)
+        print("[blue]Você venceu![/]")
         print("[yellow]Vamos jogar novamente...[/]")
+        print("[yellow]-=[/]" * 20)
+        vitoria += 1
     else:
         break
 
-print("[red]GAME OVER[/]")
+print("[red]GAME OVER![/]")
+print(f"[blue]Você venceu {vitoria} vezes![/]")
