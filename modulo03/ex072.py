385acb6b59a58abbcaf3ffb9d8b99d3ca0e4ec60
klaytonmelo <klaytonmelo09@gmail.com>
2026-08-06 22:02:35 -0300

    Update ex072

--- modulo03/ex072.py
+++ modulo03/ex072.py
@@ -7,15 +7,33 @@ e mostralo por extenso
 """
 from rich import print
 
-extenso = ("Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Onze", "Doze", "Treze", "catorze", "Quinze", "Dezeseis", "Dezesete", "Dezoito", "Dezenove", "Vinte")
+extenso = ("Zero", "Um", "Dois", "Três", "Quatro", "Cinco", "Seis", "Sete", "Oito", "Nove", "Dez", "Onze", "Doze", "Treze", "catorze", "Quinze", "Dezesseis", "Dezessete", "Dezoito", "Dezenove", "Vinte")
 
-n = int(input('Digite um numero entre 0 e 20: '))
-
-
-while n not in range(0, 30):
-    print("[red]Tente novamente...[/]")
+while True:
     n = int(input('Digite um numero entre 0 e 20: '))
 
-print("[blue]-[/]" * 30)
-print(f"Você digitou o número [green]{extenso[n]}[/]")
-print("[blue]-[/]" * 30)
+    while n not in range(0, 21):
+        print("[red]Tente novamente...[/]")
+        n = int(input('Digite um numero entre 0 e 20: '))
+
+    print("[blue]-[/]" * 30)
+    print(f"Você digitou o número [green]{extenso[n]}[/]")
+    print("[blue]-[/]" * 30)
+
+    op = str(input('Quer continuar ?: '))
+
+    """while op.strip().upper()[0] not in "SN":
+        print("[red]ERRO! tente novamente[/]")
+        op = str(input('Quer continuar ?: '))"""
+
+    while True:
+        if op.strip().upper()[0] not in "SN":
+            print("[red]ERRO! tente novamente[/]")
+            op = str(input('Quer continuar ?: '))
+        else:
+            break
+
+    if op.strip().upper()[0] == "N":
+        break
+
+print("[blue]Programa finalizado![/]")
