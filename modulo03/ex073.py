c0376c69f12c4f8934542dfbda2136adf78e1656
klaytonmelo <klaytonmelo09@gmail.com>
2026-08-07 11:38:36 -0300

    ex073 finalizado!

--- modulo03/ex073.py
+++ modulo03/ex073.py
@@ -9,34 +9,34 @@ D)Em que posição está o time Chapecoense.
 
 """
 from rich import print
-colocados = ("palmeiras", "Flamengo", "Athletico-PR", "Fluminense", "Bahia", "Red Bull Bragantino", "Cruzeiro", "Botafogo", "Corinthias", "Ceará", "Grêmio", "Internacional", "Vasco da Gama", "Mirassol", "Atlético-MG", "São Paulo", "Santos", "Chapecoense", "Coritiba", "Remo")
+times = ("Palmeiras", "Flamengo", "Athletico-PR", "Fluminense", "Bahia", "Red Bull Bragantino", "Cruzeiro", "Botafogo", "Corinthias", "Ceará", "Grêmio", "Internacional", "Vasco da Gama", "Mirassol", "Atlético-MG", "São Paulo", "Santos", "Chapecoense", "Coritiba", "Remo")
 
 print("[green]-[/]" * 30)
 print("[blue]lista de times do Brasileirão: [/]")
-for n, c in enumerate(colocados):
+for n, c in enumerate(times):
     print(f"{n + 1}. {c}")
 
 print("[green]-[/]" * 30)
 
 print("[blue]Os cinco primeiros são: [/]")
-for n, c in enumerate(colocados[:5]):
+for n, c in enumerate(times[:5]):
     print(f"{n + 1}. {c}")
 
 print("-" * 30)
 
 print("[blue]Os últimos 4 colocados são: [/]")
 
-for c in colocados[-4:]:
-    print(f"{colocados.index(c) + 1}. {c}")
+for c in times[-4:]:
+    print(f"{times.index(c) + 1}. {c}")
 
 print("-" * 30)
 
 print("[blue]Times em ordem alfabética: [/]")
-for c in sorted(colocados):
+for c in sorted(times):
     print(c)
 
 print("[green]-[/]" * 30)
 
-print(f"Chapecoense está na posição: {colocados.index("Chapecoense") + 1}")
+print(f"Chapecoense está na posição: {times.index("Chapecoense") + 1}")
 
 print("[green]-[/]" * 30)
